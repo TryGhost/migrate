3ff097edeaeaba20fb907b5fdd259afa97f5ba54
Hannah Wolfe <erisds@gmail.com>
2018-10-19 23:10:44 +0100

    ✨ Remove HR if its the first thing
    - medium exports often have an HR at the start of the content
    - we don't want to import this, so throw it away!

--- packages/mg-medium-export/lib/process-content.js
+++ packages/mg-medium-export/lib/process-content.js
@@ -1,5 +1,14 @@
 const $ = require('cheerio');
 
 module.exports = (content) => {
-    return $(content).html().trim();
+    let $content = $(content);
+
+    // Medium usually has an hr divider at the very beginning of the content
+    // We don't need this so remove it if we find it
+    let $firstSection = $content.find('.section--first');
+    if ($firstSection.children().first().hasClass('section-divider')) {
+        $firstSection.children().first().remove();
+    }
+
+    return $content.html().trim();
 };
