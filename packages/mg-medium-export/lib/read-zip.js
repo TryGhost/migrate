5df4f0e545fc2d53281db8d424d065108241cde7
Hannah Wolfe <erisds@gmail.com>
2018-10-19 22:18:21 +0100

    🐛 Handle flat medium exports
    - assume that any html files in the current folder are posts
    - this is because sometimes we just get a flat folder full of posts, rather than a neat set of named folders

--- packages/mg-medium-export/lib/read-zip.js
+++ packages/mg-medium-export/lib/read-zip.js
@@ -20,6 +20,11 @@ module.exports = (zipPath) => {
                 name: zipEntry.name,
                 html: zipEntry.getData().toString('utf8')
             });
+        } else if (/[^/].*\.html$/.test(zipEntry.entryName)) {
+            content.posts.push({
+                name: zipEntry.name,
+                html: zipEntry.getData().toString('utf8')
+            });
         }
     });
 
