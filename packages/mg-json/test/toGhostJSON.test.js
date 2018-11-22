ae80c73b4868998595ceeaa3610b375481bef389
Hannah Wolfe <erisds@gmail.com>
2018-11-22 13:57:07 +0000

    🛠 Started on mg-json test suite
    - this is just cos it's easier to test fine behaviour with a unit test than with manual testing
    - want to make sure the various weird cases of the data we get from medium all work

---
+++ packages/mg-json/test/toGhostJSON.test.js
@@ -0,0 +1,44 @@
+// Switch these lines once there are useful utils
+const testUtils = require('./utils');
+
+// Thing we are testing
+const toGhostJSON = require('../lib/to-ghost-json');
+
+// Temp utility
+const inspect = (title, obj) => console.log(title, require('util').inspect(obj, false, null)); // eslint-disable-line no-console
+
+describe('toGhostJSON', function () {
+    it('Calculates relations when it only has a post', function () {
+        const input = require(testUtils.fixturesFilename('single-post-only.json'));
+        const output = toGhostJSON(input);
+
+        output.should.be.GhostJSON();
+        output.data.posts.should.be.an.Array().with.lengthOf(1);
+        output.data.users.should.be.an.Array().with.lengthOf(1);
+
+        output.data.posts[0].authors.should.eql([output.data.users[0].id]);
+    });
+
+    it.skip('Calculates relations with both post and users', function () {
+        const input = require(testUtils.fixturesFilename('single-post-author.json'));
+
+        inspect('input', input);
+
+        const output = toGhostJSON(input);
+
+        inspect('output', output);
+
+        output.should.be.GhostJSON();
+    });
+
+    it.skip('Calculates relations across multiple posts', function () {
+        const input = require(testUtils.fixturesFilename('multi-post-only.json'));
+
+        inspect('input', input);
+
+        const output = toGhostJSON(input);
+
+        inspect('output', output);
+        output.should.be.GhostJSON();
+    });
+});
--- packages/mg-json/test/utils/assertions.js
+++ packages/mg-json/test/utils/assertions.js
@@ -4,8 +4,64 @@
  * Add any custom assertions to this file.
  */
 
-// Example Assertion
-// should.Assertion.add('ExampleAssertion', function () {
-//     this.params = {operator: 'to be a valid Example Assertion'};
-//     this.obj.should.be.an.Object;
-// });
+should.Assertion.add('GhostPost', function () {
+    this.params = {operator: 'to be a valid Ghost Post'};
+    this.obj.should.be.an.Object();
+    this.obj.should.not.have.properties(['url', 'data']);
+    this.obj.should.have.properties(['slug', 'title', 'status']); // bare minimum properties to be useful
+
+    // Relationships...
+
+    // medium only ever has a single author, but we still convert to Ghost's expected format
+    this.obj.should.have.property('authors');
+    this.obj.authors.should.be.an.Array();
+    this.obj.authors.length.should.eql(1);
+    this.obj.authors[0].should.be.a.Number().and.be.above(0);
+
+    // @TODO: tags
+    // there can be mutliple tags, but also no tags
+    // if (this.obj.tags) {
+    //     this.obj.tags.should.be.an.Array();
+    //     this.obj.tags.forEach(tag => {
+    //         tag.should.be.a.Number().and.be.above(0);
+    //     });
+    // }
+});
+
+should.Assertion.add('GhostUser', function () {
+    this.params = {operator: 'to be a valid Ghost User'};
+    this.obj.should.be.an.Object();
+    this.obj.should.not.have.properties(['url', 'data']);
+    this.obj.should.have.properties(['slug', 'name']);
+});
+
+should.Assertion.add('GhostTag', function () {
+    this.params = {operator: 'to be a valid Ghost Tag'};
+    this.obj.should.be.an.Object();
+    this.obj.should.not.have.properties(['url', 'data']);
+    this.obj.should.have.properties(['slug', 'name']);
+});
+
+should.Assertion.add('GhostJSON', function () {
+    this.params = {operator: 'to be a valid Ghost JSON'};
+    this.obj.should.be.an.Object();
+    this.obj.should.have.properties(['meta', 'data']);
+    this.obj.meta.should.be.an.Object();
+    this.obj.meta.should.have.properties(['exported_on', 'version']);
+
+    // @TODO: tags
+    //this.obj.data.should.have.properties(['posts', 'users', 'tags']);
+    this.obj.data.should.have.properties(['posts', 'users']);
+
+    // posts
+    this.obj.data.posts.should.be.an.Array();
+    this.obj.data.posts.forEach(post => post.should.be.a.GhostPost());
+
+    // users
+    this.obj.data.users.should.be.an.Array();
+    this.obj.data.users.forEach(user => user.should.be.a.GhostUser());
+
+    // @TODO: tags
+    // this.obj.data.tags.should.be.an.Array();
+    // this.obj.data.tags.forEach(tag => tag.should.be.a.GhostTag());
+});
