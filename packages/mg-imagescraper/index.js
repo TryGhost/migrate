d5a6000472719af976c783d2acaf822cdb6d7db8
Hannah Wolfe <erisds@gmail.com>
2019-02-08 16:03:26 +0000

    ✨ Update data with new paths after storing images
    - make sure that our image paths will be correct after importing

--- packages/mg-imagescraper/index.js
+++ packages/mg-imagescraper/index.js
@@ -3,6 +3,9 @@ const cheerio = require('cheerio');
 const url = require('url');
 const got = require('got');
 
+// @TODO: get rid of this!
+const path = require('path');
+
 // @TODO: expand this list
 const htmlFields = ['html'];
 
@@ -16,14 +19,18 @@ class ImageScraper {
 
     async downloadImage(src) {
         let imageUrl = url.parse(src);
+        // @TODO: don't hardcode the image path here!
+        let finalPath = path.join('/', 'content', 'images', imageUrl.pathname);
+
 
         if (this.fileCache.hasFile(imageUrl.pathname, 'image')) {
-            return imageUrl.pathname;
+            return finalPath;
         }
 
         try {
             const response = await got(src);
             this.fileCache.writeImageFile(response.body, {filename: imageUrl.pathname});
+            return finalPath;
         } catch (error) {
             console.error(error.response.body); /* eslint-disable-line no-console */
         }
@@ -31,37 +38,48 @@ class ImageScraper {
 
     async processHTML(html) {
         let $ = cheerio.load(html);
-        let images = $('img');
 
-        _.forEach(images, async (image) => {
-            let src = $(image).attr('src') === undefined ? $(image).attr('data-src') : $(image).attr('src');
-            // @TODO: replace old src with new src
-            await this.downloadImage(src);
-        });
+        const promises = $('img').map(async (i, el) => {
+            let $image = $(el);
+            let type = $image.attr('src') === undefined ? 'data-src' : 'src';
+            let newSrc = await this.downloadImage($image.attr(type));
+            $image.attr(type, newSrc);
+        }).get();
+
+        await Promise.all(promises);
+        return $.html();
     }
 
     async processField(field, value) {
         if (isImageField(field)) {
             // field has image in the name, value is null or a valid image URL
             // @TODO: fetch value & then update value
+            return value;
         } else if (isHTMLField(field)) {
             // field is an html field, we need to process the whole field looking for images
-            await this.processHTML(value);
+            return await this.processHTML(value);
         }
+
+        return value;
     }
 
     async fetch(json) {
+        let promises = [];
+
         // For each resource type e.g. posts, users
         _.forEach(json.data, (resources) => {
             // For each individual resource
             _.forEach(resources, (resource) => {
                 // For each field
-                _.forEach(resource, async (value, field) => {
-                    return await this.processField(field, value);
+                _.forEach(resource, (value, field) => {
+                    promises.push((async () => {
+                        resource[field] = await this.processField(field, value);
+                    })());
                 });
             });
         });
 
+        await Promise.all(promises);
         return json;
     }
 }
