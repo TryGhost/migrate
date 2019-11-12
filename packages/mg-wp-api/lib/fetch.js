99f5d4b7004b74f27b5929462a20274286ad0507
Hannah Wolfe <erisds@gmail.com>
2019-11-12 14:18:37 +0700

    Add caching to WP API responses
    - By caching the API responses, we can now safely rerun the migration over and over
    - This allows us to work around WP timing out by just... doing it again
    - If new content is published, it won't get migrated, but that's ok

--- packages/mg-wp-api/lib/fetch.js
+++ packages/mg-wp-api/lib/fetch.js
@@ -13,28 +13,28 @@ module.exports.discover = async (url) => {
     };
 };
 
-module.exports.all = async (url) => {
-    const api = await this.discover(url);
+const cachedFetch = async (fileCache, api, type, perPage, page) => {
+    let filename = `wp_api_${type}_${perPage}_${page}.json`;
 
-    const result = {
-        posts: [],
-        pages: []
-    };
-
-    for (let page = 1; page < api.totals.posts; page++) {
-        result.posts.concat(await api.site.posts().perPage(perPage).page(page).embed());
+    if (fileCache.hasFile(filename, 'tmp')) {
+        return await fileCache.readTmpJSONFile(filename);
     }
 
-    return result;
+    let response = await api.site[type]().perPage(perPage).page(page).embed();
+
+    await fileCache.writeTmpJSONFile(response, filename);
+
+    return response;
 };
 
-const buildTasks = (tasks, api, type) => {
+const buildTasks = (fileCache, tasks, api, type) => {
     for (let page = 1; page <= api.batches[type]; page++) {
-    // for (let page = 1; page < 2; page++) {
         tasks.push({
             title: `Fetching ${type}, page ${page} of ${api.batches[type]}`,
             task: async (ctx) => {
-                ctx.result[type] = ctx.result[type].concat(await api.site[type]().perPage(perPage).page(page).embed());
+                let response = await cachedFetch(fileCache, api, type, perPage, page);
+
+                ctx.result[type] = ctx.result[type].concat(response);
             }
         });
     }
@@ -49,8 +49,8 @@ module.exports.tasks = async (url, ctx) => {
         pages: []
     };
 
-    buildTasks(tasks, api, 'posts');
-    buildTasks(tasks, api, 'pages');
+    buildTasks(ctx.fileCache, tasks, api, 'posts');
+    buildTasks(ctx.fileCache, tasks, api, 'pages');
 
     return tasks;
 };
