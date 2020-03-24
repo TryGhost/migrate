c03cd7d6049471565b187566383e33866b59276a
Aileen Nowak <AileenCGN@gmail.com>
2020-03-24 22:55:46 +1300

    ⬆️  Update dependencies
    no issue
    
    - We can safely update got@10 now, as our migrate server has been update to Node 10
    - Also update the other dependencies, except Cheerio

--- packages/mg-imagescraper/lib/ImageScraper.js
+++ packages/mg-imagescraper/lib/ImageScraper.js
@@ -50,10 +50,8 @@ class ImageScraper {
         }
 
         try {
-            // Got 9 requires encoding: null, Got 10 requires responseType: 'buffer'
-            // @TODO: fix this when we can upgrade Got to 10 again (e.g. support Node 10 only)
             // Timeout after 20 seconds
-            let response = await got(src, {encoding: null, responseType: 'buffer', timeout: 20000});
+            let response = await got(src, {responseType: 'buffer', timeout: 20000});
             await this.fileCache.writeImageFile(response.body, imageOptions);
             return imageFile.outputPath;
         } catch (error) {
