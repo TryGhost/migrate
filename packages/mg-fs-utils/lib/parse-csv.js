893948e3fae307afefb774516adee0486e9036b9
Aileen Nowak <AileenCGN@gmail.com>
2020-08-14 16:28:35 +1200

    Allow options to be passed to csv parser

--- packages/mg-fs-utils/lib/parse-csv.js
+++ packages/mg-fs-utils/lib/parse-csv.js
@@ -1,8 +1,8 @@
 const parse = require('csv-parse');
 const fs = require('fs-extra');
 
-module.exports = (filePath) => {
-    const parser = parse({skip_lines_with_error: true, columns: true, skip_empty_lines: true});
+module.exports = (filePath, options = {skip_lines_with_error: true, columns: true, skip_empty_lines: true}) => {
+    const parser = parse(options);
     const data = [];
     return new Promise((resolve, reject) => {
         const fileStream = fs.createReadStream(filePath);
