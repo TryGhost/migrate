1958d85b1993c65241fda477ac36c0b03470269b
Aileen Nowak <AileenCGN@gmail.com>
2020-08-14 16:59:54 +1200

    💩 First pass on refactor members csv migrator

--- packages/mg-substack-members-csv/lib/converter.js
+++ packages/mg-substack-members-csv/lib/converter.js
@@ -2,7 +2,7 @@ const Promise = require('bluebird');
 const csvParser = require('csv-parser');
 const _ = require('lodash');
 const fs = require('fs-extra');
-const {formatCSV} = require('.');
+const {formatCSV} = require('./format-csv');
 
 const normalizeCSVFileToJSON = async (options) => {
     const columnsToExtract = options.columnsToExtract || [];
@@ -87,7 +87,42 @@ const normalizeMembersCSV = async (options) => {
     return fs.writeFile(resultFilePath, normalizedCSV);
 };
 
+const convertCSV = async (originFilePath, destinationFilePath) => {
+    await normalizeMembersCSV({
+        path: originFilePath,
+        destination: destinationFilePath,
+        columnsToMap: [{
+            from: 'email_disabled',
+            to: 'subscribed_to_emails',
+            negate: true
+        }, {
+            from: 'stripe_connected_customer_id',
+            to: 'stripe_customer_id'
+        }],
+        columnsToExtract: [{
+            name: 'email',
+            lookup: /^email/i
+        }, {
+            name: 'name',
+            lookup: /name/i
+        }, {
+            name: 'note',
+            lookup: /note/i
+        }, {
+            name: 'subscribed_to_emails',
+            lookup: /subscribed_to_emails/i
+        }, {
+            name: 'stripe_customer_id',
+            lookup: /stripe_customer_id/i
+        }, {
+            name: 'complimentary_plan',
+            lookup: /complimentary_plan/i
+        }]
+    });
+};
+
 module.exports = {
     normalizeCSVFileToJSON: normalizeCSVFileToJSON,
-    normalizeMembersCSV: normalizeMembersCSV
+    normalizeMembersCSV: normalizeMembersCSV,
+    convertCSV: convertCSV
 };
---
+++ packages/migrate/sources/substack-members.js
@@ -0,0 +1,53 @@
+const makeTaskRunner = require('../lib/task-runner');
+const fsUtils = require('@tryghost/mg-fs-utils');
+const csvIngest = require('@tryghost/mg-substack-members-csv');
+
+/**
+ * getTasks: Steps to Migrate subscribers from Substack
+ *
+ * Wiring of the steps to migrate subscribers from Substack.
+ *
+ * @param {String} pathToFile
+ * @param {Object} options
+ */
+module.exports.getTaskRunner = (pathToFile, options) => {
+    let tasks = [
+        {
+            title: 'Initialising',
+            task: (ctx) => {
+                ctx.options = options;
+
+                // 0. Prep a file cache for the work we are about to do.
+                ctx.fileCache = new fsUtils.FileCache(pathToFile);
+            }
+        },
+        {
+            title: 'Read csv file(s) and process with given options',
+            task: async (ctx) => {
+                // 1. Read the csv file
+                try {
+                    ctx.result = await csvIngest(ctx);
+                    await ctx.fileCache.writeTmpJSONFile(ctx.result, 'csv-members-data.csv');
+                } catch (error) {
+                    ctx.errors.push(error);
+                    throw error;
+                }
+            }
+        },
+        {
+            title: 'Write Ghost members import CSV batch files',
+            task: async (ctx) => {
+                // 8. Write a valid Ghost import zip
+                try {
+                    await ctx.fileCache.writeGhostMembersFiles(ctx);
+                } catch (error) {
+                    ctx.errors.push(error);
+                    throw error;
+                }
+            }
+        }
+    ];
+
+    // Configure a new Listr task manager, we can use different renderers for different configs
+    return makeTaskRunner(tasks, Object.assign({topLevel: true}, options));
+};
