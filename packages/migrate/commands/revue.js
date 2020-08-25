503611f15d94e68a353d9d5dd33b2cc3cebfc2e5
Aileen Nowak <AileenCGN@gmail.com>
2020-08-25 18:45:32 +1200

    💩 Added Revue commands and task lists

---
+++ packages/migrate/commands/revue.js
@@ -0,0 +1,77 @@
+const revue = require('../sources/revue');
+const ui = require('@tryghost/pretty-cli').ui;
+
+// Internal ID in case we need one.
+exports.id = 'revue';
+
+exports.group = 'Sources:';
+
+// The command to run and any params
+exports.flags = 'revue <apitoken>';
+
+// Description for the top level command
+exports.desc = 'Migrate from Revue using the API';
+
+// Descriptions for the individual params
+exports.paramsDesc = ['Revue API Token'];
+
+// Configure all the options
+exports.setup = (sywac) => {
+    sywac.boolean('-V --verbose', {
+        defaultValue: false,
+        desc: 'Show verbose output'
+    });
+    sywac.boolean('-z, --zip', {
+        defaultValue: true,
+        desc: 'Create a zip file (set to false to skip)'
+    });
+    sywac.enumeration('-s --scrape', {
+        choices: ['all', 'web', 'img', 'none'],
+        defaultValue: 'all',
+        desc: 'Configure scraping tasks'
+    });
+    sywac.string('-e --email', {
+        defaultValue: false,
+        desc: 'Provide an email domain for users e.g. mycompany.com'
+    });
+    sywac.boolean('-I, --info', {
+        defaultValue: false,
+        desc: 'Show Revue API info only'
+    });
+    sywac.boolean('--fallBackHTMLCard', {
+        defaultValue: false,
+        desc: 'Fall back to convert to HTMLCard, if standard Mobiledoc convert fails'
+    });
+};
+
+// What to do when this command is executed
+exports.run = async (argv) => {
+    let timer = Date.now();
+    let context = {errors: []};
+
+    if (argv.verbose) {
+        ui.log.info(`${argv.info ? 'Fetching info' : 'Migrating'} from Revue site`);
+    }
+
+    try {
+        // Fetch the tasks, configured correctly according to the options passed in
+        let migrate = revue.getTaskRunner(argv);
+
+        // Run the migration
+        await migrate.run(context);
+
+        if (argv.info && context.info) {
+            let batches = context.info.batches.posts;
+            ui.log.info(`Batch info: ${context.info.totals.posts} posts ${batches} batches.`);
+        }
+
+        if (argv.verbose) {
+            ui.log.info('Done', require('util').inspect(context.result.data, false, 2));
+        }
+    } catch (error) {
+        ui.log.info('Done with errors', context.errors);
+    }
+
+    // Report success
+    ui.log.ok(`Successfully written output to ${context.outputFile} in ${Date.now() - timer}ms.`);
+};
