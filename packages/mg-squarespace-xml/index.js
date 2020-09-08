c3dae3858d01f1822ae54254c9985dd10f876fbd
Aileen Nowak <AileenCGN@gmail.com>
2020-09-08 17:23:28 +1200

    Fixed normalizing not being fulfilled

--- packages/mg-squarespace-xml/index.js
+++ packages/mg-squarespace-xml/index.js
@@ -6,11 +6,10 @@ module.exports = async (ctx) => {
     const input = await fs.readFile(ctx.options.pathToFile, 'utf-8');
 
     // normalize xml file (posts, pages, users, tags)
-    const normalized = normalize(input, ctx);
+    const normalized = await normalize(input, ctx);
 
     // process the content
     const processed = await process(normalized, ctx);
-    // console.log('processed', processed);
 
     return processed;
 };
