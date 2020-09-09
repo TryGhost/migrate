4a9587cbc559f4f41ffa8b7278c3ad63134468a5
Aileen Nowak <AileenCGN@gmail.com>
2020-09-09 18:26:52 +1200

    More refactors

--- packages/mg-squarespace-xml/index.js
+++ packages/mg-squarespace-xml/index.js
@@ -1,15 +1,11 @@
 const fs = require('fs').promises;
-const normalize = require('./lib/normalize-xml');
 const process = require('./lib/process');
 
 module.exports = async (ctx) => {
     const input = await fs.readFile(ctx.options.pathToFile, 'utf-8');
 
-    // normalize xml file (posts, pages, users, tags)
-    const normalized = await normalize.all(input, ctx);
-
-    // process the content
-    const processed = await process(normalized, ctx);
+    // process xml file (posts, pages, users, tags)
+    const processed = await process.all(input, ctx);
 
     return processed;
 };
