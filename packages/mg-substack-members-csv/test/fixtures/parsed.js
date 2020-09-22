508965e43dfdf8af23a4442f97f18a7ee51e2a6f
Aileen Nowak <AileenCGN@gmail.com>
2020-09-22 20:45:06 +1200

    Added fixtures for tests

---
+++ packages/mg-substack-members-csv/test/fixtures/parsed.js
@@ -0,0 +1,388 @@
+const free = [
+    {
+        email: 'herroooo@substack.com',
+        active_subscription: 'true',
+        expiry: '2021-07-06T16: 20: 44.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15: 06: 27.713Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'testing@substacks.com',
+        active_subscription: 'true',
+        expiry: '2021-08-02T19: 01: 30.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15: 02: 18.223Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'maguirec086@comm.ca',
+        active_subscription: 'true',
+        expiry: '2025-06-23T23: 41: 42.980Z',
+        email_disabled: 'true',
+        created_at: '2018-08-29T20: 50: 03.435Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'tcowenaldkuf@gmu.edu',
+        active_subscription: 'true',
+        expiry: '2118-09-11T12: 14: 56.557Z',
+        email_disabled: 'false',
+        created_at: '2018-09-08T00: 19: 14.583Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'Bob.Something@advisers.co.uk',
+        active_subscription: 'true',
+        expiry: '2020-12-31T19: 58: 06.194Z',
+        email_disabled: 'false',
+        created_at: '2020-01-01T19: 58: 29.925Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'ömil@gmail.com',
+        active_subscription: 'true',
+        expiry: '2020-10-05T21: 23: 16.080Z',
+        email_disabled: 'false',
+        created_at: '2020-07-07T21: 23: 16.083Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'christine_blackling@smallpond.com',
+        active_subscription: 'true',
+        expiry: '2021-01-04T23: 26: 30.533Z',
+        email_disabled: 'false',
+        created_at: '2020-01-05T23: 26: 30.536Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'jill@example.com',
+        active_subscription: 'true',
+        expiry: '2021-05-23T18: 28: 09.830Z',
+        email_disabled: 'false',
+        created_at: '2020-05-23T18: 28: 09.834Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'harry_potter@gmail.com',
+        active_subscription: 'true',
+        expiry: '2020-08-18T19: 05: 42.465Z',
+        email_disabled: 'false',
+        created_at: '2020-05-20T16: 52: 22.654Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'albert.a.dumfield@gov.mt',
+        active_subscription: 'true',
+        expiry: '2020-10-20T18: 04: 09.000Z',
+        email_disabled: 'false',
+        created_at: '2018-10-04T18: 02: 01.817Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'johnlholiday@gmail.com',
+        active_subscription: 'true',
+        expiry: '2021-04-12T21: 29: 31.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15: 37: 23.469Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'edwinjeans@gmail.com',
+        active_subscription: 'true',
+        expiry: '2021-01-13T15: 50: 53.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15: 22: 11.139Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'someNews@gmail.com',
+        active_subscription: 'true',
+        expiry: '2020-12-27T02: 27: 57.000Z',
+        email_disabled: 'false',
+        created_at: '2018-12-11T02: 25: 36.560Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'nottimferris@gmail.com',
+        active_subscription: 'true',
+        expiry: '2021-04-19T02: 13: 33.000Z',
+        email_disabled: 'true',
+        created_at: '2018-08-27T15: 36: 25.539Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'e271828@pacobell.net',
+        active_subscription: 'true',
+        expiry: '2021-01-12T12: 59: 18.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15: 33: 45.828Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'kirstygouda@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2018-12-19T08: 03: 04.977Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'albus_dumbledore@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2018-12-22T13: 17: 25.800Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'patrickstarfish@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2018-12-25T20: 43: 22.178Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'rgdigest@gmail.com',
+        active_subscription: 'true',
+        expiry: '2020-10-20T18: 24: 42.000Z',
+        email_disabled: 'false',
+        created_at: '2018-10-04T18: 18: 52.533Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'elpaper@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2019-08-18T13: 36: 31.230Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'maïbes@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2020-08-06T10: 21: 25.605Z',
+        stripe_customer_id: null,
+        type: 'free'
+    }
+];
+
+const subscribed = [
+    {
+        email: 'herroooo@substack.com',
+        active_subscription: 'true',
+        expiry: '2021-07-06T16:20:44.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15:06:27.713Z',
+        stripe_customer_id: 'cus_D5JgW66mn4j0BQ',
+        type: 'paid'
+    },
+    {
+        email: 'testing@substacks.com',
+        active_subscription: 'true',
+        expiry: '2021-08-02T19:01:30.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15:02:18.223Z',
+        stripe_customer_id: 'cus_DFTNc7a0BTd9FL',
+        type: 'paid'
+    },
+    {
+        email: 'maguirec086@comm.ca',
+        active_subscription: 'true',
+        expiry: '2025-06-23T23:41:42.980Z',
+        email_disabled: 'true',
+        created_at: '2018-08-29T20:50:03.435Z',
+        stripe_customer_id: '',
+        type: 'comp'
+    },
+    {
+        email: 'tcowenaldkuf@gmu.edu',
+        active_subscription: 'true',
+        expiry: '2118-09-11T12:14:56.557Z',
+        email_disabled: 'false',
+        created_at: '2018-09-08T00:19:14.583Z',
+        stripe_customer_id: 'cus_DZFvu2haq10CTS',
+        type: 'comp'
+    },
+    {
+        email: 'Bob.Something@advisers.co.uk',
+        active_subscription: 'true',
+        expiry: '2020-12-31T19:58:06.194Z',
+        email_disabled: 'false',
+        created_at: '2020-01-01T19:58:29.925Z',
+        stripe_customer_id: '',
+        type: 'comp'
+    },
+    {
+        email: 'ömil@gmail.com',
+        active_subscription: 'true',
+        expiry: '2020-10-05T21:23:16.080Z',
+        email_disabled: 'false',
+        created_at: '2020-07-07T21:23:16.083Z',
+        stripe_customer_id: '',
+        type: 'gift'
+    },
+    {
+        email: 'christine_blackling@smallpond.com',
+        active_subscription: 'true',
+        expiry: '2021-01-04T23:26:30.533Z',
+        email_disabled: 'false',
+        created_at: '2020-01-05T23:26:30.536Z',
+        stripe_customer_id: '',
+        type: 'gift'
+    },
+    {
+        email: 'jill@example.com',
+        active_subscription: 'true',
+        expiry: '2021-05-23T18:28:09.830Z',
+        email_disabled: 'false',
+        created_at: '2020-05-23T18:28:09.834Z',
+        stripe_customer_id: '',
+        type: 'gift'
+    },
+    {
+        email: 'harry_potter@gmail.com',
+        active_subscription: 'true',
+        expiry: '2020-08-18T19:05:42.465Z',
+        email_disabled: 'false',
+        created_at: '2020-05-20T16:52:22.654Z',
+        stripe_customer_id: '',
+        type: 'gift'
+    },
+    {
+        email: 'albert.a.dumfield@gov.mt',
+        active_subscription: 'true',
+        expiry: '2020-10-20T18:04:09.000Z',
+        email_disabled: 'false',
+        created_at: '2018-10-04T18:02:01.817Z',
+        stripe_customer_id: 'cus_Dj3FYJk1b6lDFY',
+        type: 'paid'
+    },
+    {
+        email: 'johnlholiday@gmail.com',
+        active_subscription: 'true',
+        expiry: '2021-04-12T21:29:31.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15:37:23.469Z',
+        stripe_customer_id: 'cus_84talUs8FYit4C',
+        type: 'paid'
+    },
+    {
+        email: 'edwinjeans@gmail.com',
+        active_subscription: 'true',
+        expiry: '2021-01-13T15:50:53.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15:22:11.139Z',
+        stripe_customer_id: 'cus_9pOU8iQ5irkMzA',
+        type: 'paid'
+    },
+    {
+        email: 'someNews@gmail.com',
+        active_subscription: 'true',
+        expiry: '2020-12-27T02:27:57.000Z',
+        email_disabled: 'false',
+        created_at: '2018-12-11T02:25:36.560Z',
+        stripe_customer_id: 'cus_E8HVwNa7yDwSK6',
+        type: 'paid'
+    },
+    {
+        email: 'nottimferris@gmail.com',
+        active_subscription: 'true',
+        expiry: '2021-04-19T02:13:33.000Z',
+        email_disabled: 'true',
+        created_at: '2018-08-27T15:36:25.539Z',
+        stripe_customer_id: 'cus_8B4DJZYcx59mic',
+        type: 'paid'
+    },
+    {
+        email: 'e271828@pacobell.net',
+        active_subscription: 'true',
+        expiry: '2021-01-12T12:59:18.000Z',
+        email_disabled: 'false',
+        created_at: '2018-08-27T15:33:45.828Z',
+        stripe_customer_id: 'cus_9oyUvFMhVLzHbb',
+        type: 'paid'
+    },
+    {
+        email: 'kirstygouda@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2018-12-19T08:03:04.977Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'albus_dumbledore@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2018-12-22T13:17:25.800Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'patrickstarfish@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2018-12-25T20:43:22.178Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'rgdigest@gmail.com',
+        active_subscription: 'true',
+        expiry: '2020-10-20T18:24:42.000Z',
+        email_disabled: 'false',
+        created_at: '2018-10-04T18:18:52.533Z',
+        stripe_customer_id: 'cus_Dj3abd10gY5MNo',
+        type: 'paid'
+    },
+    {
+        email: 'elpaper@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2019-08-18T13:36:31.230Z',
+        stripe_customer_id: null,
+        type: 'free'
+    },
+    {
+        email: 'maïbes@gmail.com',
+        active_subscription: '',
+        expiry: '',
+        email_disabled: 'false',
+        created_at: '2020-08-06T10:21:25.605Z',
+        stripe_customer_id: null,
+        type: 'free'
+    }
+];
+
+module.exports = {
+    free: free,
+    subscribed: subscribed
+};
