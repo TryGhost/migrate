b89beebb333a1ca40bedd55b1f235a895b2b4bd4
Aileen Nowak <AileenCGN@gmail.com>
2020-09-30 12:59:05 +1300

    🐛 Fixed csv parser being called wrong

--- packages/mg-substack-csv/index.js
+++ packages/mg-substack-csv/index.js
@@ -1,9 +1,9 @@
-const parse = require('@tryghost/mg-fs-utils/lib/csv').parse;
+const csv = require('@tryghost/mg-fs-utils').csv;
 const map = require('./lib/mapper');
 const process = require('./lib/process');
 
 module.exports = async (ctx) => {
-    const input = await parse(ctx.options.pathToFile);
+    const input = await csv.parse(ctx.options.pathToFile);
     const mapped = await map(input, ctx.options);
     const processed = await process(mapped, ctx);
 
