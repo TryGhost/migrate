094dee66980be37508dbf53f11a49d429c0feb9a
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-02-02 17:19:32 +0000

    Enforce maximum string length on more metadata values

--- packages/mg-json/lib/to-ghost-json/validate.js
+++ packages/mg-json/lib/to-ghost-json/validate.js
@@ -2,14 +2,39 @@ module.exports = (json) => {
     json.posts.forEach((item, index) => {
         let input = item.data;
 
+        // String length data from https://github.com/TryGhost/Ghost/blob/main/core/server/data/schema/schema.js
         // @TODO: log some sort of warning for things like this?
 
         if (input.custom_excerpt && input.custom_excerpt.length > 300) {
             input.custom_excerpt = input.custom_excerpt.substring(0, 300);
         }
 
+        if (input.og_title && input.og_title.length > 300) {
+            input.og_title = input.og_title.substring(0, 300);
+        }
+
+        if (input.og_description && input.og_description.length > 500) {
+            input.og_description = input.og_description.substring(0, 500);
+        }
+
+        if (input.twitter_title && input.twitter_title.length > 300) {
+            input.twitter_title = input.twitter_title.substring(0, 300);
+        }
+
+        if (input.twitter_description && input.twitter_description.length > 500) {
+            input.twitter_description = input.twitter_description.substring(0, 500);
+        }
+
+        if (input.meta_title && input.meta_title.length > 300) {
+            input.meta_title = input.meta_title.substring(0, 300);
+        }
+
         if (input.meta_description && input.meta_description.length > 500) {
             input.meta_description = input.meta_description.substring(0, 500);
+        }
+
+        if (input.email_subject && input.email_subject.length > 300) {
+            input.email_subject = input.email_subject.substring(0, 300);
         }
 
         if (input.feature_image_alt && input.feature_image_alt.length > 125) {
