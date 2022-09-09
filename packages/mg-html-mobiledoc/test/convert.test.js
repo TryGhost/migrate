8abe2201c1190101bf7efd61385e93536eeb3913
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-09-09 19:47:28 +0100

    Upgrade @tryghost/html-to-mobiledoc

--- packages/mg-html-mobiledoc/test/convert.test.js
+++ packages/mg-html-mobiledoc/test/convert.test.js
@@ -88,7 +88,7 @@ describe('Convert', function () {
         mobiledoc.should.be.an.Object().with.properties('version', 'markups', 'atoms', 'cards', 'sections');
         mobiledoc.version.should.eql('0.3.1');
         mobiledoc.atoms.should.eql([['soft-return', '', {}]]);
-        mobiledoc.cards.should.eql([['image',{src: 'https://example.com/image.jpg', alt: 'Hello', title: ''}],['hr',{}]]);
+        mobiledoc.cards.should.eql([['image',{src: 'https://example.com/image.jpg', alt: 'Hello'}],['hr',{}]]);
         mobiledoc.markups.should.eql([['i'],['a',['href','https://example.com']]]);
         mobiledoc.sections.should.eql([[1,'h2',[[0,[],0,'Good stuff here']]],[10,0],[1,'p',[[0,[],0,'Hello '],[0,[0],1,'world']]],[10,1],[1,'p',[[0,[],0,'Link to '],[0,[1],1,'Example']]],[1,'p',[[0,[],0,'Hello '],[1,[],0,0],[0,[],0,'world']]]]);
     });
@@ -120,5 +120,41 @@ describe('Convert', function () {
         mobiledoc.cards.should.eql([['button',{alignment: 'center', buttonUrl: '#/portal/signup', buttonText: 'Subscribe'}],['button',{alignment: 'center', buttonUrl: '#/portal/signup',buttonText: 'Subscribe'}],['button',{alignment: 'center', buttonUrl: '/#/portal/signup', buttonText: 'Subscribe'}],['button',{alignment: 'center', buttonUrl: 'https://example.com/#/portal/signup/free', buttonText: 'Subscribe'}]]);
         mobiledoc.markups.should.eql([['a',['href','#/portal/signup']],['a',['href','/#/portal/signup']],['a',['href','https://example.com/#/portal/signup/free']]]);
         mobiledoc.sections.should.eql([[10,0],[1,'p',[[0,[],0,'Please '],[0,[0],1,'Subscribe']]],[10,1],[1,'p',[[0,[],0,'Please '],[0,[0],1,'Subscribe']]],[10,2],[1,'p',[[0,[],0,'Please '],[0,[1],1,'Subscribe']]],[10,3],[1,'p',[[0,[],0,'Please '],[0,[2],1,'Subscribe']]]]);
+    });
+
+    it('Correctly converts a linked image', function () {
+        let post = {
+            html: '<a href="https://example.com"><img src="https://example.com/images/photo.jpg" /></a>'
+        };
+        const htmlCard = false;
+
+        convertPost(post, htmlCard);
+
+        const mobiledoc = JSON.parse(post.mobiledoc);
+
+        mobiledoc.should.be.an.Object().with.properties('version', 'markups', 'atoms', 'cards', 'sections');
+        mobiledoc.version.should.eql('0.3.1');
+        mobiledoc.atoms.should.eql([]);
+        mobiledoc.cards.should.eql([['image',{src: 'https://example.com/images/photo.jpg', href: 'https://example.com/'}]]);
+        mobiledoc.markups.should.eql([]);
+        mobiledoc.sections.should.eql([[10,0]]);
+    });
+
+    it('Correctly converts a linked image with alt & title text', function () {
+        let post = {
+            html: '<a href="https://example.com"><img src="https://example.com/images/photo.jpg" alt="My alt text" title="My title" /></a>'
+        };
+        const htmlCard = false;
+
+        convertPost(post, htmlCard);
+
+        const mobiledoc = JSON.parse(post.mobiledoc);
+
+        mobiledoc.should.be.an.Object().with.properties('version', 'markups', 'atoms', 'cards', 'sections');
+        mobiledoc.version.should.eql('0.3.1');
+        mobiledoc.atoms.should.eql([]);
+        mobiledoc.cards.should.eql([['image',{src: 'https://example.com/images/photo.jpg', alt: 'My alt text', href: 'https://example.com/', title: 'My title'}]]);
+        mobiledoc.markups.should.eql([]);
+        mobiledoc.sections.should.eql([[10,0]]);
     });
 });
