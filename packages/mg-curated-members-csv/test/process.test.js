ce9f8456aae634e337f6a09830e27784abf2ed7d
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-09-28 17:37:52 +0100

    Use Jest in @tryghost/mg-curated-members-csv

--- packages/mg-curated-members-csv/test/process.test.js
+++ packages/mg-curated-members-csv/test/process.test.js
@@ -1,11 +1,9 @@
+/* eslint no-undef: 0 */
 const path = require('path');
 const parseMembers = require('../');
 
-// Switch these lines once there are useful utils
-require('./utils');
-
 describe('Parses Curated subscribers file', function () {
-    it('only signups', async function () {
+    test('only signups', async function () {
         const ctx = {
             options: {
                 pathToFile: path.resolve('./test/fixtures/subscribers.csv')
@@ -15,22 +13,22 @@ describe('Parses Curated subscribers file', function () {
         const result = await parseMembers(ctx);
         const subs = result.free;
 
-        subs.length.should.equal(20);
+        expect(subs).toBeArrayOfSize(20);
 
-        subs[0].email.should.eql('herroooo@curated.com');
-        subs[0].subscribed_to_emails.should.eql(true);
-        subs[0].complimentary_plan.should.eql(false);
-        should.not.exist(subs[0].stripe_customer_id);
-        subs[0].created_at.toISOString().should.eql('2021-02-04T07:55:20.000Z');
-        should.not.exist(subs[0].expiry);
-        subs[0].type.should.eql('free');
+        expect(subs[0].email).toEqual('herroooo@curated.com');
+        expect(subs[0].subscribed_to_emails).toEqual(true);
+        expect(subs[0].complimentary_plan).toEqual(false);
+        expect(subs[0].stripe_customer_id).toBeNull();
+        expect(subs[0].created_at.toISOString()).toEqual('2021-02-04T07:55:20.000Z');
+        expect(subs[0].expiry).toBeNull();
+        expect(subs[0].type).toEqual('free');
 
-        subs[19].email.should.eql('maïbes@gmail.com');
-        subs[19].subscribed_to_emails.should.eql(true);
-        subs[19].complimentary_plan.should.eql(false);
-        should.not.exist(subs[19].stripe_customer_id);
-        subs[19].created_at.toISOString().should.eql('2021-01-30T13:54:19.000Z');
-        should.not.exist(subs[19].expiry);
-        subs[19].type.should.eql('free');
+        expect(subs[19].email).toEqual('maïbes@gmail.com');
+        expect(subs[19].subscribed_to_emails).toEqual(true);
+        expect(subs[19].complimentary_plan).toEqual(false);
+        expect(subs[19].stripe_customer_id).toBeNull();
+        expect(subs[19].created_at.toISOString()).toEqual('2021-01-30T13:54:19.000Z');
+        expect(subs[19].expiry).toBeNull();
+        expect(subs[19].type).toEqual('free');
     });
 });
