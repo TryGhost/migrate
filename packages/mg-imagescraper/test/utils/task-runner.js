58970a57cbc0a56808448bfb4b18ffe5fce8dc9d
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-10-10 12:47:49 +0100

    Include task runner file in each package that uses it for tests
    This is temporary until all packages are ESM

---
+++ packages/mg-imagescraper/test/utils/task-runner.js
@@ -0,0 +1,20 @@
+const Listr = require('listr');
+const smartRenderer = require('@tryghost/listr-smart-renderer');
+
+module.exports = (tasks, options = {}) => {
+    let nonVerboseRenderer = options.renderer || smartRenderer;
+
+    options.renderer = options.verbose ? 'verbose' : nonVerboseRenderer;
+    options.exitOnError = options.exitOnError || false;
+    options.concurrent = options.concurrent !== undefined ? options.concurrent : 3;
+
+    // Allow a simple flag to be passed in to set "top level" settings
+    if (options.topLevel) {
+        // Top level tasks should exit on error, subtasks should report errors, not exit entirely
+        options.exitOnError = true;
+        // Top level tasks should execute one-after-the-other, most subtasks can have some concurrency
+        options.concurrent = false;
+    }
+
+    return new Listr(tasks, options);
+};
