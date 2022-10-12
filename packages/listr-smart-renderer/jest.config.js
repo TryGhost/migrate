af770d0300e9f43b9dd4c1d13458de0d5275a8b5
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-10-12 18:04:03 +0100

    Fix local imports for chalk

--- packages/listr-smart-renderer/jest.config.js
+++ packages/listr-smart-renderer/jest.config.js
@@ -1,6 +1,10 @@
 // Extra assertions are from https://github.com/jest-community/jest-extended
 export default async () => {
     return {
-        setupFilesAfterEnv: ['jest-extended/all']
+        setupFilesAfterEnv: ['jest-extended/all'],
+        moduleNameMapper: {
+            '#ansi-styles': '<rootDir>/node_modules/chalk/source/vendor/ansi-styles/index.js',
+            '#supports-color': '<rootDir>/node_modules/chalk/source/vendor/supports-color/index.js'
+        }
     };
 };
