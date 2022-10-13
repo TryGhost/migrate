652900b2537a06efd4d1115cd672f2751517bc59
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-10-13 10:48:36 +0100

    Revert "Fix local imports for chalk"
    This reverts commit af770d0300e9f43b9dd4c1d13458de0d5275a8b5.

--- packages/listr-smart-renderer/jest.config.js
+++ packages/listr-smart-renderer/jest.config.js
@@ -1,10 +1,6 @@
 // Extra assertions are from https://github.com/jest-community/jest-extended
 export default async () => {
     return {
-        setupFilesAfterEnv: ['jest-extended/all'],
-        moduleNameMapper: {
-            '#ansi-styles': '<rootDir>/node_modules/chalk/source/vendor/ansi-styles/index.js',
-            '#supports-color': '<rootDir>/node_modules/chalk/source/vendor/supports-color/index.js'
-        }
+        setupFilesAfterEnv: ['jest-extended/all']
     };
 };
