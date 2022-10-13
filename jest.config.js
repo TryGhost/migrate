48fdaac2162da770f339c2c79cc2d6957627882e
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-10-13 21:35:10 +0100

    Upgrade Chalk to 5.1.2

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 // Extra assertions are from https://github.com/jest-community/jest-extended
+import {join} from 'node:path';
+import {URL} from 'node:url';
+const __dirname = new URL('.', import.meta.url).pathname;
+
 export default async () => {
     return {
         verbose: true,
-        setupFilesAfterEnv: ['jest-extended/all']
+        setupFilesAfterEnv: ['jest-extended/all'],
+        resolver: join(__dirname, 'test/utils/resolver.cjs')
     };
 };
