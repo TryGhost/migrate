891d5a31fc6f348e940df8e78f283585b1f6314c
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-10-13 11:04:27 +0100

    Improve readability of cli.js

--- packages/migrate/bin/cli.js
+++ packages/migrate/bin/cli.js
@@ -1,57 +1,42 @@
 #!/usr/bin/env node
 import prettyCLI from '@tryghost/pretty-cli';
 
+import cacheCommands from '../commands/cache.js';
+import curatedMembersCommands from '../commands/curated-members.js';
+import curatedCommands from '../commands/curated.js';
+import ghostCommands from '../commands/ghost.js';
+import hubspotCommands from '../commands/hubspot.js';
+import jekyllCommands from '../commands/jekyll.js';
+import jsonEmailCommands from '../commands/json/email.js';
+import jsonHtmlCardCommands from '../commands/json/html-card.js';
+import jsonHtmlCommands from '../commands/json/html.js';
+import jsonSlugifyCommands from '../commands/json/slugify.js';
+import jsonCommands from '../commands/json.js';
+import mediumCommands from '../commands/medium.js';
+import revueCommands from '../commands/revue.js';
+import squarespaceCommands from '../commands/squarespace.js';
+import substackMembersCommands from '../commands/substack-members.js';
+import substackCommands from '../commands/substack.js';
+import wpApiCommands from '../commands/wp-api.js';
+
 prettyCLI.preface('Command line utilities for migrating content to Ghost.');
 
-import cacheCommands from '../commands/cache.js';
 prettyCLI.command(cacheCommands);
-
-import curatedMembersCommands from '../commands/curated-members.js';
 prettyCLI.command(curatedMembersCommands);
-
-import curatedCommands from '../commands/curated.js';
 prettyCLI.command(curatedCommands);
-
-import ghostCommands from '../commands/ghost.js';
 prettyCLI.command(ghostCommands);
-
-import hubspotCommands from '../commands/hubspot.js';
 prettyCLI.command(hubspotCommands);
-
-import jekyllCommands from '../commands/jekyll.js';
 prettyCLI.command(jekyllCommands);
-
-import jsonEmailCommands from '../commands/json/email.js';
 prettyCLI.command(jsonEmailCommands);
-
-import jsonHtmlCardCommands from '../commands/json/html-card.js';
 prettyCLI.command(jsonHtmlCardCommands);
-
-import jsonHtmlCommands from '../commands/json/html.js';
 prettyCLI.command(jsonHtmlCommands);
-
-import jsonSlugifyCommands from '../commands/json/slugify.js';
 prettyCLI.command(jsonSlugifyCommands);
-
-import jsonCommands from '../commands/json.js';
 prettyCLI.command(jsonCommands);
-
-import mediumCommands from '../commands/medium.js';
 prettyCLI.command(mediumCommands);
-
-import revueCommands from '../commands/revue.js';
 prettyCLI.command(revueCommands);
-
-import squarespaceCommands from '../commands/squarespace.js';
 prettyCLI.command(squarespaceCommands);
-
-import substackMembersCommands from '../commands/substack-members.js';
 prettyCLI.command(substackMembersCommands);
-
-import substackCommands from '../commands/substack.js';
 prettyCLI.command(substackCommands);
-
-import wpApiCommands from '../commands/wp-api.js';
 prettyCLI.command(wpApiCommands);
 
 prettyCLI.style({
