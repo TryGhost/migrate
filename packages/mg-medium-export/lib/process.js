ea9f839ac29049c9e87ed215386047dcf8557fec
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2023-01-10 09:42:17 +0000

    Fix Medium author attribution

--- packages/mg-medium-export/lib/process.js
+++ packages/mg-medium-export/lib/process.js
@@ -4,8 +4,8 @@ import processProfile from './process-profile.js';
 export default (input) => {
     let output = {};
 
-    if (input.profile) {
-        output.users = [processProfile(input.profile)];
+    if (input.profile || input.profiles) {
+        output.users = [processProfile(input.profile || input.profiles)];
     }
 
     let globalUser = output.users && output.users.length === 1 ? output.users[0] : null;
