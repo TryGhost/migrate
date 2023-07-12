6085df9d84a699c18e8b073ed844deec3357979d
Simon Backx <simon@ghost.org>
2023-07-12 13:02:48 +0200

    Fixed Stripe importer to avoid charging for created subscriptions immediately
    no issue

--- packages/mg-stripe-csv/src/test/importing.test.ts
+++ packages/mg-stripe-csv/src/test/importing.test.ts
@@ -5,7 +5,7 @@ import {createCouponImporter} from '../lib/importers/createCouponImporter.js';
 import {createPriceImporter} from '../lib/importers/createPriceImporter.js';
 import {createProductImporter} from '../lib/importers/createProductImporter.js';
 import {createSubscriptionImporter} from '../lib/importers/createSubscriptionImporter.js';
-import {buildPrice, buildProduct, buildSubscription, createValidCustomer, getStripeTestAPIKey} from './utils/stripe.js';
+import {buildPrice, buildProduct, buildSubscription, createDeclinedCustomer, createValidCustomer, getStripeTestAPIKey} from './utils/stripe.js';
 import {Options} from '../lib/Options.js';
 import assert from 'assert/strict';
 
@@ -16,6 +16,12 @@ describe('test', () => {
     let stats: ImportStats;
     let subscriptionImporter: ReturnType<typeof createSubscriptionImporter>;
     let validCustomer: Stripe.Customer;
+    let declinedCustomer: Stripe.Customer;
+
+    beforeAll(async () => {
+        validCustomer = await createValidCustomer(stripe.client);
+        declinedCustomer = await createDeclinedCustomer(stripe.client);
+    });
 
     beforeEach(async () => {
         stats = new ImportStats();
@@ -48,9 +54,6 @@ describe('test', () => {
             priceImporter,
             couponImporter
         });
-
-        // Create a valid customer
-        validCustomer = await createValidCustomer(stripe.client);
     });
 
     it('Basic migration', async () => {
@@ -128,7 +131,7 @@ describe('test', () => {
         assert.equal(newSubscription.items.data[0].quantity, 1);
     });
 
-    it.only('Subsciption that renews today', async () => {
+    it('Subsciption that renews today', async () => {
         const oldProduct: Stripe.Product = buildProduct({});
 
         const oldPrice: Stripe.Price = buildPrice({
@@ -156,15 +159,57 @@ describe('test', () => {
         assert.equal(newSubscription.status, 'active');
         assert.equal(newSubscription.start_date, oldSubscription.start_date);
 
-        // Check current period end is extended by ±30 days
-        assert.ok(newSubscription.current_period_end > oldSubscription.current_period_end + 29 * 24 * 60 * 60);
-        assert.ok(newSubscription.current_period_end < oldSubscription.current_period_end + 32 * 24 * 60 * 60);
+        // Check current period end is extended to 1 hour from now to avoid immediate billing
+        assert.ok(newSubscription.current_period_end < Math.ceil(Date.now() / 1000) + 3600 + 60);
+        assert.ok(newSubscription.current_period_end > Math.ceil(Date.now() / 1000) + 3600 - 60);
 
         assert.equal(newSubscription.trial_end, oldSubscription.trial_end);
         assert.equal(newSubscription.cancel_at_period_end, oldSubscription.cancel_at_period_end);
         assert.equal(newSubscription.customer, validCustomer.id);
         assert.equal(newSubscription.description, oldSubscription.description);
         assert.equal(newSubscription.default_payment_method, validCustomer.invoice_settings.default_payment_method);
+        assert.equal(newSubscription.items.data.length, 1);
+        assert.equal(newSubscription.items.data[0].price.metadata.importOldId, oldPrice.id);
+        assert.equal(newSubscription.items.data[0].quantity, 1);
+    });
+
+    it('Subsciption that renews today and will be declined', async () => {
+        const oldProduct: Stripe.Product = buildProduct({});
+
+        const oldPrice: Stripe.Price = buildPrice({
+            product: oldProduct,
+            recurring: {
+                interval: 'month'
+            }
+        });
+
+        const oldSubscription: Stripe.Subscription = buildSubscription({
+            customer: declinedCustomer.id,
+            items: [
+                {
+                    price: oldPrice
+                }
+            ],
+            current_period_end: Math.ceil(Date.now() / 1000)
+        });
+
+        const newSubscriptionId = await subscriptionImporter.recreate(oldSubscription);
+        const newSubscription = await stripe.client.subscriptions.retrieve(newSubscriptionId);
+
+        // Do some basic assertions
+        assert.equal(newSubscription.metadata.importOldId, oldSubscription.id);
+        assert.equal(newSubscription.status, 'active');
+        assert.equal(newSubscription.start_date, oldSubscription.start_date);
+
+        // Check current period end is extended to 1 hour from now to avoid immediate billing
+        assert.ok(newSubscription.current_period_end < Math.ceil(Date.now() / 1000) + 3600 + 60);
+        assert.ok(newSubscription.current_period_end > Math.ceil(Date.now() / 1000) + 3600 - 60);
+
+        assert.equal(newSubscription.trial_end, oldSubscription.trial_end);
+        assert.equal(newSubscription.cancel_at_period_end, oldSubscription.cancel_at_period_end);
+        assert.equal(newSubscription.customer, declinedCustomer.id);
+        assert.equal(newSubscription.description, oldSubscription.description);
+        assert.equal(newSubscription.default_payment_method, declinedCustomer.invoice_settings.default_payment_method);
         assert.equal(newSubscription.items.data.length, 1);
         assert.equal(newSubscription.items.data[0].price.metadata.importOldId, oldPrice.id);
         assert.equal(newSubscription.items.data[0].quantity, 1);
