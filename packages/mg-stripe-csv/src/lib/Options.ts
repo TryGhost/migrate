2c014e0243633268304875176c15515fe958f5be
Simon Backx <simon@ghost.org>
2023-07-18 17:30:22 +0200

    Cleaned up options (stripe)

--- packages/mg-stripe-csv/src/lib/Options.ts
+++ packages/mg-stripe-csv/src/lib/Options.ts
@@ -10,19 +10,6 @@ export class Options {
             desc: 'Run the import without actually creating any subscriptions'
         },
         {
-            type: 'number',
-            flags: '--verbose -v',
-            defaultValue: false,
-            desc: 'Print verbose output'
-        },
-        {
-            type: 'boolean',
-            // Somehow -vv is not working in sywac
-            flags: '--very-verbose',
-            defaultValue: false,
-            desc: 'Print very verbose output'
-        },
-        {
             type: 'string',
             flags: '--from',
             defaultValue: false,
@@ -35,10 +22,10 @@ export class Options {
             desc: 'The Stripe API key of the new account (optional)'
         },
         {
-            type: 'boolean',
-            flags: '--pause',
-            defaultValue: false,
-            desc: 'Pause collection of old subscriptions in the old account after they have been recreated in the new account'
+            type: 'number',
+            flags: '--delay',
+            defaultValue: 12,
+            desc: 'Period in hours in which newly created subscriptions won\'t create any charges (subscriptions that expire in this period will be delayed a bit). Within this period you should be able to confirm the migration or revert it. Defaults to 12.'
         },
         {
             type: 'boolean',
@@ -47,16 +34,17 @@ export class Options {
             desc: 'Print debug output'
         },
         {
-            type: 'string',
-            flags: '--test-clock',
-            defaultValue: null,
-            desc: 'Import subscriptions associated with a test clock'
+            type: 'number',
+            flags: '--verbose -v',
+            defaultValue: false,
+            desc: 'Print verbose output'
         },
         {
-            type: 'number',
-            flags: '--delay',
-            defaultValue: 12,
-            desc: 'Period in hours in which newly created subscriptions won\'t create any charges (subscriptions that expire in this period will be delayed a bit). Within this period you should be able to confirm the migration or revert it. Defaults to 12.'
+            type: 'boolean',
+            // Somehow -vv is not working in sywac
+            flags: '--very-verbose',
+            defaultValue: false,
+            desc: 'Print very verbose output'
         }
     ];
 
@@ -64,7 +52,6 @@ export class Options {
     verboseLevel: 0 | 1 | 2;
     oldApiKey?: string;
     newApiKey?: string;
-    pause: boolean;
     debug: boolean;
     testClock?: string;
     forceRecreate: boolean;
@@ -77,7 +64,6 @@ export class Options {
         this.verboseLevel = argv['very-verbose'] ? 2 : argv.verbose ? 1 : 0;
         this.oldApiKey = argv.from ?? undefined;
         this.newApiKey = argv.to ?? undefined;
-        this.pause = argv.pause ?? false;
         this.debug = argv.debug ?? false;
         this.testClock = argv['test-clock'] ?? undefined;
         this.forceRecreate = argv['force-recreate'] ?? false;
