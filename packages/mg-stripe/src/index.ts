af7a517266870f0b6a24bd03ba8c6f006913fab4
Sag <guptazy@gmail.com>
2023-07-21 15:29:52 +0200

    Refactored setup, keep package self-contained

--- packages/mg-stripe/src/index.ts
+++ packages/mg-stripe/src/index.ts
@@ -1,5 +1,3 @@
-export {Logger} from './lib/Logger.js';
-export {Options} from './lib/Options.js';
-export {confirm} from './lib/commands/confirm.js';
-export {revert} from './lib/commands/revert.js';
-export {copy} from './lib/commands/copy.js';
+import command from './lib/command.js';
+
+export default command;
