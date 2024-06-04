e7d127eb00078a4663e27af85594b125a8512843
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2024-06-04 10:51:59 +0100

    Update types.d.ts

--- packages/mg-context/src/types.d.ts
+++ packages/mg-context/src/types.d.ts
@@ -1,3 +1,4 @@
+declare module '@tryghost/errors';
 declare module '@tryghost/html-to-mobiledoc';
 declare module '@tryghost/mg-json';
-declare module '@tryghost/listr-smart-renderer'
+declare module '@tryghost/listr-smart-renderer';
