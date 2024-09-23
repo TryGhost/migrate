28e95dc30e2010a2fc028af5fa84c75e52bde8a6
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2024-09-23 12:29:03 +0100

    Substack: Update zip parsing to support nested folders

--- packages/mg-substack/lib/read-zip.js
+++ packages/mg-substack/lib/read-zip.js
@@ -26,16 +26,16 @@ export default (zipPath) => {
     //   - 5678.another-slug.html
 
     fsUtils.zip.read(zipPath, (entryName, zipEntry) => {
-        // Catch all HTML files inside `profile/`
-        if (/^posts\/.*\.html$/.test(entryName)) {
+        // Catch all HTML files inside `post/`
+        if (/^posts\/.*\.html$/.test(entryName) || /\/posts\/.*\.html$/.test(entryName)) {
             debug(`Found post HTML file ${entryName}`);
             content.posts.push({
                 name: entryName.replace('posts/', ''),
                 html: zipEntry.getData().toString('utf8')
             });
 
-        // Skip if not matched above
-        } else if (entryName === 'posts.csv') {
+        // Get the posts.csv
+        } else if (entryName === 'posts.csv' || entryName.endsWith('/posts.csv')) {
             debug(`Found posts CSV file ${entryName}`);
             content.csv = zipEntry.getData().toString('utf8');
         }
