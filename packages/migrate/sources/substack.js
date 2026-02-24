eeed71b8d526617ec8eeb1a5badc70724bb00dc9
Nick Moreton <nick@ghost.org>
2026-02-24 14:19:31 +0000

    Added default domains for Substack asset scraping

--- packages/migrate/sources/substack.js
+++ packages/migrate/sources/substack.js
@@ -229,7 +229,8 @@ const getTaskRunner = (options) => {
                 });
                 ctx.webScraper = new MgWebScraper(ctx.fileCache, scrapeConfig, postProcessor, skipScrape);
                 ctx.assetScraper = new MgAssetScraper(ctx.fileCache, {
-                    allowAllDomains: true
+                    allowAllDomains: true,
+                    domains: ['https://cdn.substack.com', 'https://substackcdn.com', 'https://substack-post-media.s3.amazonaws.com', 'https://api.substack.com']
                 }, ctx);
                 await ctx.assetScraper.init();
                 ctx.linkFixer = new MgLinkFixer();
