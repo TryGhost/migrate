cc3cac55a11fb139a5ca04d3f428f4a4dada9bee
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2026-02-26 09:24:48 +0000

    jest > node:test for listr-smart-renderer

--- packages/listr-smart-renderer/test/utils.test.js
+++ packages/listr-smart-renderer/test/utils.test.js
@@ -1,3 +1,5 @@
+import assert from 'node:assert/strict';
+import {describe, it} from 'node:test';
 import {taskNumber} from '../lib/utils.js';
 
 describe('Task Number', function () {
@@ -5,45 +7,45 @@ describe('Task Number', function () {
     let tasks;
 
     const taskNumberShouldEql = (result) => {
-        return expect(taskNumber(index, tasks)).toEqual(result);
+        return assert.equal(taskNumber(index, tasks), result);
     };
 
-    test('Correctly outputs a simple task number in the format x/y', function () {
+    it('Correctly outputs a simple task number in the format x/y', function () {
         index = 0;
         tasks = new Array(3); // Fake tasks array
 
         taskNumberShouldEql('1/3');
     });
 
-    test('Can pad a task number correctly in the format 0x/yy', function () {
+    it('Can pad a task number correctly in the format 0x/yy', function () {
         index = 3;
         tasks = new Array(14); // Fake tasks array
 
         taskNumberShouldEql('04/14');
     });
 
-    test('Can pad a task number correctly in the format xx/yy', function () {
+    it('Can pad a task number correctly in the format xx/yy', function () {
         index = 11;
         tasks = new Array(14); // Fake tasks array
 
         taskNumberShouldEql('12/14');
     });
 
-    test('Can pad a task number correctly in the format 00x/yyy', function () {
+    it('Can pad a task number correctly in the format 00x/yyy', function () {
         index = 3;
         tasks = new Array(111); // Fake tasks array
 
         taskNumberShouldEql('004/111');
     });
 
-    test('Can pad a task number correctly in the format 0xx/yyy', function () {
+    it('Can pad a task number correctly in the format 0xx/yyy', function () {
         index = 11;
         tasks = new Array(111); // Fake tasks array
 
         taskNumberShouldEql('012/111');
     });
 
-    test('Can pad a task number correctly in the format xxx/yyy', function () {
+    it('Can pad a task number correctly in the format xxx/yyy', function () {
         index = 100;
         tasks = new Array(111); // Fake tasks array
 
