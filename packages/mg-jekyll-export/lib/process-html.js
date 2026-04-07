7aef259d3f92dfc9d124bd77159b64f4c1de1997
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2026-04-07 19:47:38 +0100

    Use mg-utils in mg-jekyll-export

--- packages/mg-jekyll-export/lib/process-html.js
+++ packages/mg-jekyll-export/lib/process-html.js
@@ -1,4 +1,6 @@
-import * as cheerio from 'cheerio';
+import {domUtils} from '@tryghost/mg-utils';
+
+const {serializeChildren} = domUtils;
 
 /*
 Process the HTML of a single Jekyll Post.
@@ -7,60 +9,54 @@ Receives raw HTML, returns processed HTML
 */
 
 export default (rawHtml, options = {}) => {
-    const $html = cheerio.load(rawHtml, {
-        xml: {
-            xmlMode: false,
-            decodeEntities: false,
-            scriptingEnabled: false
+    return domUtils.processFragment(rawHtml, (parsed) => {
+        // Convert relative links and image paths to absolute
+        if (options.url) {
+            const urlData = new URL(options.url);
+            // The "origin" the URL truncated to the domain.
+            // The difference between the "href" matters for blogs that were hosted
+            // in sub-directories so that relative and relative-to-root URLs resolve correctly.
+            const urlOrigin = urlData.origin;
+
+            parsed.$('a').forEach((anchor) => {
+                const thisURL = anchor.getAttribute('href');
+
+                // If it starts with a slash, append the base URL
+                if (thisURL && thisURL.indexOf('/') === 0) {
+                    const updatedURL = `${urlOrigin.replace(/^\/|\/$/g, '')}/${thisURL.replace(/^\/|\/$/g, '')}`;
+                    anchor.setAttribute('href', updatedURL);
+                }
+            });
+
+            parsed.$('img').forEach((img) => {
+                const thisSrc = img.getAttribute('src');
+
+                // If it starts with a slash, append the base URL
+                if (thisSrc && thisSrc.indexOf('/') === 0) {
+                    const updatedURL = `${urlOrigin.replace(/^\/|\/$/g, '')}/${thisSrc.replace(/^\/|\/$/g, '')}`;
+                    img.setAttribute('src', updatedURL);
+                }
+            });
         }
-    }, false); // This `false` is `isDocument`. If `true`, <html>, <head>, and <body> elements are introduced
 
-    // Convert relative links and image paths to absolute
-    if (options.url) {
-        const urlData = new URL(options.url);
-        // The "origin" the URL truncated to the domain.
-        // The difference between the "href" matters for blogs that were hosted
-        // in sub-directories so that relative and relative-to-root URLs resolve correctly.
-        const urlOrigin = urlData.origin;
+        // Unwrap <p> tags that are in <li> tags
+        parsed.$('li').forEach((li) => {
+            const p = li.querySelector('p');
 
-        $html('a').each((i, anchor) => {
-            const thisURL = $html(anchor).attr('href');
-
-            // If it starts with a slash, append the base URL
-            if (thisURL && thisURL.indexOf('/') === 0) {
-                const updatedURL = `${urlOrigin.replace(/^\/|\/$/g, '')}/${thisURL.replace(/^\/|\/$/g, '')}`;
-                $html(anchor).attr('href', updatedURL);
+            if (p) {
+                li.innerHTML = serializeChildren(p);
             }
         });
 
-        $html('img').each((i, img) => {
-            const thisSrc = $html(img).attr('src');
+        // convert HTML back to a string
+        let html = parsed.html();
 
-            // If it starts with a slash, append the base URL
-            if (thisSrc && thisSrc.indexOf('/') === 0) {
-                const updatedURL = `${urlOrigin.replace(/^\/|\/$/g, '')}/${thisSrc.replace(/^\/|\/$/g, '')}`;
-                $html(img).attr('src', updatedURL);
-            }
-        });
-    }
+        // Remove empty attributes
+        html = html.replace(/=""/g, '');
 
-    // Unwrap <p> tags that are in <li> tags
-    $html('li').each((i, li) => {
-        const hasP = $html(li).find('p').length;
+        // Trim whitespace
+        html = html.trim();
 
-        if (hasP) {
-            $html(li).html($html(li).find('p').html());
-        }
+        return html;
     });
-
-    // convert HTML back to a string
-    let html = $html.html();
-
-    // Remove empty attributes
-    html = html.replace(/=""/g, '');
-
-    // Trim whitespace
-    html = html.trim();
-
-    return html;
 };
